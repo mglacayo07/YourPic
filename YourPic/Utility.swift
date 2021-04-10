6ab5fe5f74b87809e52a01425d7dce626096e86d
Maria Lacayo <mg.lacayo07@gmail.com>
2021-04-10 12:44:38 -0500

    login controller done

---
+++ YourPic/Utility.swift
@@ -0,0 +1,19 @@
+//
+//  Utility.swift
+//  YourPic
+//
+//  Created by Maria Lacayo on 10/04/21.
+//
+
+import UIKit
+
+class Utility: UIViewController{
+    
+    func alert(title:String,message:String){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "ok", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+}
--- YourPic/ViewController.swift
+++ YourPic/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import Firebase
 
-class ViewController: UIViewController {
+class ViewController: Utility {
 
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func createAccount(_ sender: Any) {
+       
+    }
+    
+    @IBAction func login(_ sender: Any) {
+        print("Entre a login")
+        let mail = email.text!
+        let pwd = password.text!
+        if(mail.count==0){
+            self.alert(title: "Error de credenciales", message: "Correo Invalido")
+            return
+        }
+        if(pwd.count==0){
+            print("PWD VACIO")
+            self.alert(title: "Error de credenciales", message: "Contraseña Invalida")
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: mail, password: pwd) { (result, error) in
+            if let error = error{
+                self.alert(title: "Error al autenticar", message: "Error en las credenciales")
+                print("Error \(error)")
+            }else{
+                print("Usuario autenticado: \(result?.user.uid)")
+            }
+        }
+    }
+    
 }
 
