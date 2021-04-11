c8c593cff18a170e9f4b0ab334174be5a6b539d1
Maria Lacayo <mg.lacayo07@gmail.com>
2021-04-10 21:25:41 -0500

    logout

--- YourPic/MainViewController.swift
+++ YourPic/MainViewController.swift
@@ -6,24 +6,44 @@
 //
 
 import UIKit
+import Firebase
 
 class MainViewController: UIViewController {
-
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.hidesBackButton = true
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logout(_ sender: Any) {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let logout = UIAlertAction(title: "Cerrar Sesión", style: UIAlertAction.Style.default) { (action) in
+            print("ME CIERRO")
+            do {
+                try Auth.auth().signOut()
+                self.navigationController?.popViewController(animated: true)
+                self.dismiss(animated: true, completion: nil)
+            }catch{
+                print("MURIO")
+            }
+        }
+        
+        let cancel = UIAlertAction(title: "Cancelar", style: UIAlertAction.Style.cancel) { (action) in
+            print("CAncelar")
+            
+        }
+        
+        actionSheet.addAction(logout)
+        actionSheet.addAction(cancel)
+        
+        self.present(actionSheet, animated: true, completion: nil)
+        
     }
-    */
+    
+    @IBAction func addImage(_ sender: Any) {
+        
+    }
+    
 
 }
